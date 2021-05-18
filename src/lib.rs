330c4fedb527408ac161b9ccc3cd8ec0e29a2ee7
cyberrumor <cyberrumor@icloud.com>
2021-05-18 15:56:29 -0600

    branching trees

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use std::fs;
 use std::collections::HashMap;
 
-pub fn tokenize_words(corpus: String) -> Vec<String> {
+pub fn tokenize_words(corpus: &str) -> Vec<String> {
     let mut result: Vec::<String> = Vec::new();
     for word in corpus.to_lowercase().split_whitespace() {
         result.push(word.to_string());
@@ -9,63 +9,183 @@ pub fn tokenize_words(corpus: String) -> Vec<String> {
     result
 }
 
-pub fn tokenize_sent(corpus: String) -> Vec<String> {
+pub fn tokenize_sent(corpus: &str) -> Vec<String> {
     let mut result: Vec::<String> = Vec::new();
-    for sentence in corpus.split(".") {
-        result.push(sentence.to_string());
+    // collect words until the result is a sentence
+    let mut sentence: String = String::new();
+    let splits = corpus.split_whitespace();
+    let mut words: Vec<String> = Vec::new();
+    for i in splits {
+        words.push(i.to_string());
+    }
+    let mut index: usize = 0;
+    while index < words.len() {
+        // add a space if sentence isn't empty
+        if !sentence.is_empty() {
+            sentence.push(' ');
+        }
+        // add current word
+        sentence.push_str(&words[index]);
+        // prep for the next word
+        index += 1;
+        // reset our bools
+        let mut punctuation: bool = false;
+
+        // identify if the sentence ends with punctuation.
+        if sentence.ends_with('.') {
+            let second_to_last: usize = &words[index - 1].chars().count() - 2;
+            if sentence.ends_with("Mr.")
+            | sentence.ends_with("Mrs.")
+            | sentence.ends_with("Ms.")
+            | sentence.ends_with("Dr.")
+            | sentence.ends_with("Mr.")
+            // if it ends with a period and isn't a title, the last leter before the period should
+            // be lowercase. Otherwise, it must be a middle initial or an acronym.
+            | &words[index - 1].clone().chars().nth(second_to_last).unwrap().is_uppercase() {
+                punctuation = false;
+            } else {
+                // it's a valid end-of-sentence period.
+                punctuation = true;
+            }
+
+        } else if sentence.ends_with('?')
+        | sentence.ends_with('!')
+        | sentence.ends_with(")\"")
+        | sentence.ends_with('\"')
+        | sentence.ends_with('”')
+        | sentence.ends_with(")”") {
+            punctuation = true;
+        }
+
+        // a sentence needs balanced parentheses.
+        if sentence.matches('(').count() != sentence.matches(')').count() {
+            continue;
+        }
+
+        // if the sentence doesn't end with punctation, keep going.
+        if !punctuation {
+            continue;
+        }
+
+        // a sentence must have a matching number of quotes.
+        if sentence.matches('"').count() % 2 != 0 {
+            continue;
+        }
+
+        // the first letter of the next word should be uppercase.
+        if index < words.len() && words[index].chars().next().unwrap().is_lowercase() {
+            continue;
+        }
+
+        // If we've reached the bottom, we have a full sentence.
+        // Push it into the results and clear it.
+        result.push(sentence.clone());
+        sentence.clear();
     }
     result
 }
 
-pub fn gen_frequency(all_words: &Vec<String>, sample_length: usize) -> HashMap<String, usize> {
-    let mut frequency_dict: HashMap<String, usize> = HashMap::new();
 
-    // prime
+pub fn strip_nonalpha(corpus: &str) -> String {
+    let mut result: String = String::new();
+    for character in corpus.chars() {
+        if character.is_alphabetic() || character.is_whitespace() {
+            result.push(character);
+        }
+    }
+    result
+}
+
+
+pub fn gen_frequency(all_words: &[String], sample_length: usize) -> HashMap<String, usize> {
+    let mut frequency_dict: HashMap<String, usize> = HashMap::new();
     let mut x: usize = 0;
     let mut y: usize = sample_length;
-    let mut last_progress: usize = 0;
-    // this loop populates frequency_dict
     while y <= all_words.len() {
-        let progress: usize = ((y as f64 / all_words.len() as f64) * 100.0) as usize;
-        if progress != last_progress {
-            eprint!("Sample_length: {:?} Progress: {:?}% \r", sample_length, progress);
-            last_progress = progress;
-        }
-        // get a slice of sample_length words
         let slice = &all_words[x..y];
         let mut phrase = String::new();
         for word in slice {
             phrase.push_str(word);
-            phrase.push_str(" ");
+            phrase.push(' ');
         }
         let key = phrase.trim_end().to_string();
-        // println!("{:?}", key);
         // insert key only if it doesn't exist yet, if it does, update it
         let count = frequency_dict.entry(key).or_insert(0);
         *count += 1;
         y += 1;
         x += 1;
     }
-    println!();
     frequency_dict
 }
 
 pub fn stringify_corpus(file: String) -> String {
-    let result = fs::read_to_string(file)
+    let corpus = fs::read_to_string(file)
         .expect("Something went wrong reading the file");
+    let result: String = corpus.replace("\n", " ");
     result
 }
 
-pub fn remove_stopwords(corpus: Vec<String>, stopwords: Vec<String>) -> Vec<String> {
-    let mut vec: Vec<String> = Vec::new();
-    for word in corpus {
-        if stopwords.contains(&word) {
-            continue;
-        }
-        vec.push(word);
+
+pub fn get_trees(sent: Vec<String>) -> Vec<Vec<String>> {
+    let mut trees: Vec<Vec<String>> = Vec::new();
+    if sent.len() < 2 {
+        trees.push(sent);
+        return trees;
     }
-    vec
+
+    for x in 0..sent.len() {
+        let mut y: usize = x + 2;
+        while y <= sent.len() {
+            let vec: Vec<String> = sent[x..y].to_vec();
+            if vec.len() > 1 {
+                trees.push(vec)
+            }
+            y += 1
+        }
+    }
+
+    trees
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,77 @@ use std::process::exit;
 use std::collections::HashMap;
 mod lib;
 
+#[derive(Debug, Clone)]
+struct Sentence {
+    original: String,
+    stripped: String,
+    tokenized: Vec<String>,
+    trees: Vec<Vec<String>>,
+    score: f32,
+}
+
 fn main() {
     let args: Vec<String> = env::args()
         .collect();
 
-    if args.len() < 1 {
-        println!("usage: bite filename1.txt filename2.txt");
+    if args.len() < 2 {
+        println!("usage: bite filename1.txt filename2.txt 3");
         exit(1);
     }
 
-    let filenames: &[String] = &args[1..];
+    let filenames: &[String] = &args[1..args.len() - 1];
+    // let summary_length: usize = args.last().unwrap().parse().unwrap();
 
-    // stringify corpi
+    // stringify corpora
     let mut corpora: String = String::new();
     for file in filenames {
         let corpus: String = lib::stringify_corpus(file.clone());
+        corpora.push_str(&" ");
         corpora.push_str(&corpus);
     }
+    
+    // split into sentences
+    let original_sentences: Vec<String> = lib::tokenize_sent(&corpora);
 
-    let words: Vec<String> = lib::tokenize_words(corpora);
-    let frequency_dict: HashMap<String, usize> = lib::gen_frequency(&words, 1);
-    let max_value: usize = frequency_dict.values().max().unwrap().clone();
-    println!("max value: {:?}", max_value);
+
+    // create sentence objects
+    let mut db: Vec<Sentence> = Vec::new();
+    for original in original_sentences {
+        let stripped: String = lib::strip_nonalpha(&original.clone().to_lowercase());
+        let tokenized: Vec<String> = lib::tokenize_words(&stripped);
+        let trees: Vec<Vec<String>> = lib::get_trees(tokenized.clone());
+        let object: Sentence = Sentence {
+            original,
+            stripped,
+            tokenized,
+            trees,
+            score: 0.,
+        };
+        db.push(object)
+    }
+
+    for s in &db {
+        println!();
+        println!("{:?}", s.original);
+        println!("{:?}", s.stripped);
+        println!("{:?}", s.tokenized);
+        println!("{:?}", s.trees);
+        println!("{:?}", s.score);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
